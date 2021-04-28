7aca121970a8f5edfb77b56e960f7eabae0998bf
jônatas Uchôa Zeferino <jonatasu_taz@hotmail.com>
2021-04-28 16:48:49 -0400

    lista

---
+++ teste/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace teste
+{
+    class Program
+    { 
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("escreva o seu nome:");
+            string nome = Console.ReadLine();
+
+            if (nome == null || string.IsNullOrEmpty(nome))
+            {
+                Console.WriteLine("nome não informado para seguir no progama");
+                return;
+            }
+
+            Console.WriteLine("irforme sua idade: ");
+            var idadeString = Console.ReadLine();
+            int idade;
+
+            bool idadeInformada = int.TryParse(idadeString, out idade);
+            if (idadeInformada == false)
+            {
+                Console.WriteLine("informe a idade válida para continuar");
+                return;
+            }
+
+            bool estaConvidado;
+
+            switch (nome)
+            {
+                case "Mateus":
+                    estaConvidado = true;
+                    break;
+                case "Carlos":
+                    estaConvidado = true;
+                    break;
+                case "João":
+                    estaConvidado = true;
+                    break;
+                case "José":
+                    estaConvidado = true;
+                    break;
+                case "Maria":
+                    estaConvidado = true;
+                    break;
+                default:
+                    estaConvidado = false;
+                    break;
+            }
+
+            if(estaConvidado == true && idade > 18)
+            {
+                Console.WriteLine("Parabéns pode aproveitar a festa");
+            }
+            else
+            {
+                Console.WriteLine("que pena! você não possui os critérios necessários para entrar(está convidado e ser maior)");
+            }
+        }
+    }
+}
