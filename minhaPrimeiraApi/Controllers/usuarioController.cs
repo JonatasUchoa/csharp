e4b9587f95077a2791a6312c8a318ce4b1638994
jônatas Uchôa Zeferino <jonatasu_taz@hotmail.com>
2021-06-01 13:29:15 -0400

    api c#

---
+++ minhaPrimeiraApi/Controllers/usuarioController.cs
@@ -0,0 +1,17 @@
+using Microfoft.AspNetCore.Mvc;
+using System;
+using System.Collection.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace minhaPrimeiraApi.Controller
+{
+    {ApiController}
+    {Route("api/[controller]")}
+    public class usuarioController : ControllerBase
+    {
+        {HttpGet}
+        public ia
+    }
+}
